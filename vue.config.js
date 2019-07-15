f2058f5010c917ab2085c73c88263f2c74639303
Luca Iaconelli <lucabelli@msn.com>
2019-07-15 13:39:46 +0200

    FIX: Public path.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/vue-cursor-fx/'
+        : '/',
     productionSourceMap: false,
     lintOnSave: true,
     pages: {
