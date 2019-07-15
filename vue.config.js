41404382d00c05019c2021f25fdcac7d23fd6893
Luca Iaconelli <lucabelli@msn.com>
2019-07-15 14:01:18 +0200

    FEAT: Added some ENV VAR.

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,20 @@
-module.exports = {
-    publicPath: (
-        process.env.NODE_ENV === 'production'
-        ? '/vue-cursor-fx/'
+
+const BASE_URL = (
+    process.env.NODE_ENV === 'production'
+        ? process.env.BASE_URL
         : '/'
-    ),
+);
+
+module.exports = {
+    publicPath: BASE_URL,
     lintOnSave: process.env.NODE_ENV !== 'production',
     productionSourceMap: false,
     pages: {
         index: {
             entry: './src/main.js',
-            title: 'Vue Cursor Fx',
+            title: process.env.title,
+            description: process.env.description,
+            base_url: BASE_URL,
             colors: {
                 primary: '#333',
             },
