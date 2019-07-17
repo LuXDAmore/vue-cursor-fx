bba1c60368b0e35871c4131e32711e59204f1bf8
Luca Iaconelli <lucabelli@msn.com>
2019-07-17 14:02:57 +0200

    FEAT: vue-markdown-loader config.

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,26 @@
 
-const BASE_URL = (
-    process.env.NODE_ENV === 'production'
+const IS_DEV = process.env.NODE_ENV !== 'production',
+ BASE_URL = (
+    ! IS_DEV
         ? process.env.BASE_URL
         : '/'
 );
 
 module.exports = {
     publicPath: BASE_URL,
-    lintOnSave: process.env.NODE_ENV !== 'production',
+    lintOnSave: IS_DEV,
     productionSourceMap: false,
     pages: {
         index: {
             entry: './src/main.js',
-            title: process.env.TITLE,
-            description: process.env.DESCRIPTION,
-            library: process.env.LIBRARY,
-            author: process.env.AUTHOR,
-            twitter: process.env.TWITTER,
-            facebook: process.env.FACEBOOK,
+            title: process.env.VUE_APP_TITLE,
+            description: process.env.VUE_APP_DESCRIPTION,
+            library: process.env.VUE_APP_LIBRARY,
+            author: process.env.VUE_APP_AUTHOR,
+            twitter: process.env.VUE_APP_TWITTER,
+            facebook: process.env.VUE_APP_FACEBOOK,
             colors: {
-                primary: '#333',
+                primary: process.env.VUE_APP_COLOR,
             },
         },
     },
@@ -27,5 +28,39 @@ module.exports = {
         output: {
             libraryExport: 'default',
         },
+    },
+    chainWebpack(
+        config
+    ) {
+
+        config.module
+            .rule(
+                'md'
+            )
+            .test(
+                /\.md/
+            )
+            .use(
+                'vue-loader'
+            )
+            .loader(
+                'vue-loader'
+            )
+            .end()
+            .use(
+                'vue-markdown-loader'
+            )
+            .loader(
+                'vue-markdown-loader/lib/markdown-compiler'
+            )
+            .options(
+                {
+                    raw: true,
+                    breaks: true,
+                    wrapper: 'article',
+                }
+            )
+        ;
+
     },
 };
