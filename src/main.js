09e5aacf7c17dbd763de16e2ed4b4b68d2dd1308
Luca Iaconelli <lucabelli@msn.com>
2019-07-17 14:03:41 +0200

    FEAT: Added library config.

--- src/main.js
+++ src/main.js
@@ -1,17 +1,30 @@
+// Vue
 import Vue from 'vue';
+
+// Skeleton
 import VApp from './VApp';
+
+// Component
+import Library from './library';
 import CursorFX from './CursorFx';
+
+// Install
+Vue.component(
+    CursorFX.name,
+    CursorFX
+);
+
+// Library data
+Vue.use(
+    Library,
+    process.env
+);
 
 // Config
 const DEBUG = process.env.NODE_ENV === 'development';
 
 Vue.config.silent = ! DEBUG;
 Vue.config.productionTip = DEBUG;
-
-Vue.component(
-    CursorFX.name,
-    CursorFX
-);
 
 new Vue(
     {
