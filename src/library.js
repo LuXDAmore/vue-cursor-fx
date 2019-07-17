cb7661b2284059b1100bbffc6b995b0d70069986
Luca Iaconelli <lucabelli@msn.com>
2019-07-17 14:03:23 +0200

    CHORE: Added library config.

---
+++ src/library.js
@@ -0,0 +1,19 @@
+/**
+ * Creates an instance of the design system
+ *
+ * @function install
+ * @param {VueInstance} Vue Vue instance.
+ */
+export default {
+    install(
+        Vue,
+        options
+    ) {
+
+        const OPTIONS = options;
+
+        Vue.library = OPTIONS;
+        Vue.prototype.$library = OPTIONS;
+
+    },
+};
