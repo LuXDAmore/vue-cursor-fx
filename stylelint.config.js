7335e3a53f6b38388e20b9fc0c18f9e94b27654e
Luca Iaconelli <lucabelli@msn.com>
2019-09-20 11:28:57 +0200

    CHORE: ignore 'grid-auto-flow' for IE.

--- stylelint.config.js
+++ stylelint.config.js
@@ -20,6 +20,7 @@ module.exports = {
                 ignore: [
                     'calc',
                     'border-radius',
+                    'grid-auto-flow',
                     'user-select-none',
                     'pointer-events',
                     'background-img-opts',
