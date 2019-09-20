fcb9e63b33d77cf4d10bfb8166fe4ec7cb59239e
Luca Iaconelli <lucabelli@msn.com>
2019-09-20 18:10:38 +0200

    FIX: Component export.

--- src/CursorFx/index.js
+++ src/CursorFx/index.js
@@ -3,9 +3,15 @@ import component from './CursorFx.vue';
 
 const plugin = {
     CursorFx: component,
+    installed: false,
     install(
         Vue
     ) {
+
+        if( plugin.installed )
+            return;
+
+        plugin.installed = true;
 
         Vue.component(
             component.name,
@@ -15,10 +21,17 @@ const plugin = {
     },
 };
 
-// To allow use as module (npm/webpack/etc.) export component
-export default plugin;
-
 // Auto install
-typeof window !== 'undefined' && window.Vue && window.Vue.use(
+let GlobalVue = null;
+
+if( typeof window !== 'undefined' )
+    GlobalVue = window.Vue;
+else if( typeof global !== 'undefined' )
+    GlobalVue = global.Vue;
+
+GlobalVue && GlobalVue.use(
     plugin
 );
+
+// To allow use as module (npm/webpack/etc.) export component
+export default plugin;
